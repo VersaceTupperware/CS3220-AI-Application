011f2cdfd71662ba4637ce3ee74c20f2a88c64d2
Oscar Herrera <herrera_o@outlook.com>
2025-10-30 04:03:37 -0700

    Project init.

---
+++ src/main/java/dev/cs3220project1/cs3220aiapplication/Cs3220AiApplication.java
@@ -0,0 +1,13 @@
+package dev.cs3220project1.cs3220aiapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs3220AiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs3220AiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cs3220project1/cs3220aiapplication/Cs3220AiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cs3220project1.cs3220aiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs3220AiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
