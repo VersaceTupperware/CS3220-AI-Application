7e358c18302e54d53f5ad1cd772b56d215548f78
VersaceTupperware <zeber187@yahoo.com>
2025-10-30 12:38:09 -0700

    I added a comment to the main application page

--- src/main/java/dev/cs3220project1/cs3220aiapplication/Cs3220AiApplication.java
+++ src/main/java/dev/cs3220project1/cs3220aiapplication/Cs3220AiApplication.java
@@ -3,6 +3,8 @@ package dev.cs3220project1.cs3220aiapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Adding a comment to the application page test commit. This comment is by Alejandro Perez
+
 @SpringBootApplication
 public class Cs3220AiApplication {
 
