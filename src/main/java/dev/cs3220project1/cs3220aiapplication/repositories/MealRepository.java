79f274a14aa17ae12cea6021b48fef61e67231df
Oscar Herrera <herrera_o@outlook.com>
2025-12-05 21:59:57 -0800

    Fixes.

--- src/main/java/dev/cs3220project1/cs3220aiapplication/repositories/MealRepository.java
+++ src/main/java/dev/cs3220project1/cs3220aiapplication/repositories/MealRepository.java
@@ -4,9 +4,13 @@ import dev.cs3220project1.cs3220aiapplication.entities.MealEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MealRepository extends JpaRepository<MealEntity, Long> {
 
     // For "My Meals" per logged-in user
-    List<MealEntity> findByUsernameOrderByCreatedAtDesc(String username);
+    List<MealEntity> findAllByUsernameOrderByCreatedAtDesc(String username);
+
+    Optional<MealEntity> findByIdAndUsername(Long id, String username);
+
 }
